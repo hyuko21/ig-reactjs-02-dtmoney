8cb6830d8afbfebf639aa10c73c42855c9992ece
Victor Wagner <hyukodev@gmail.com>
2022-01-15 19:08:20 -0300

    feat: add react-model

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import Modal from 'react-modal'
+import { useState } from 'react'
 import { GlobalStyle } from './styles/global'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModelOpen, setIsNewTransactionModelOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModelOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModelOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        ariaHideApp={false}
+        isOpen={isNewTransactionModelOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
